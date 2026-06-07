7b6f43526d0cf9d3c08160f4936030b48c63f8a0
FarrosFR <56761912+farrosfr@users.noreply.github.com>
2026-06-08 01:00:53 +0700

    Add SEO metadata and sitemap

---
+++ src/pages/robots.txt.js
@@ -0,0 +1,13 @@
+const site = "https://noa.farros.co";
+
+export function GET() {
+  return new Response(`User-agent: *
+Allow: /
+
+Sitemap: ${site}/sitemap.xml
+`, {
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8"
+    }
+  });
+}
---
+++ src/pages/sitemap.xml.js
@@ -0,0 +1,48 @@
+import { posts } from "../data/posts";
+
+const site = "https://noa.farros.co";
+const siteUpdated = "2026-06-08";
+
+const staticRoutes = [
+  { path: "/", priority: "1.0" },
+  { path: "/blog/", priority: "0.8" },
+  { path: "/quotes/", priority: "0.7" }
+];
+
+const urls = [
+  ...staticRoutes.map((route) => ({
+    loc: `${site}${route.path}`,
+    lastmod: siteUpdated,
+    changefreq: "weekly",
+    priority: route.priority
+  })),
+  ...posts.map((post) => ({
+    loc: `${site}/blog/${post.slug}/`,
+    lastmod: post.date,
+    changefreq: "monthly",
+    priority: "0.7"
+  }))
+];
+
+export function GET() {
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+  .map(
+    (url) => `  <url>
+    <loc>${url.loc}</loc>
+    <lastmod>${url.lastmod}</lastmod>
+    <changefreq>${url.changefreq}</changefreq>
+    <priority>${url.priority}</priority>
+  </url>`
+  )
+  .join("\n")}
+</urlset>`,
+    {
+      headers: {
+        "Content-Type": "application/xml; charset=utf-8"
+      }
+    }
+  );
+}
